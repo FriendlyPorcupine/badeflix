194bc9e813078e0d84fb82ca885df06116486c89
KZoubek <91133180+KZoubek@users.noreply.github.com>
2022-09-08 15:38:56 +0200

    added profile html for delete and update

---
+++ client/profile.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('#signin-form');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector('#email').value;
+  const password = document.querySelector('#password').value;
+
+  try {
+    const {data} = await axios.delete('v1/user/user', {
+      email,
+      password
+    });
+
+    window.location.href = "/";
+
+  } catch(_) {
+    Toastify({
+      text: "User deleted!",
+      duration: 3000,
+      backgroundColor: "red",
+    }).showToast();
+  }
+});
