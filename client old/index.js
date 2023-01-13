2a66ef3bf668325845a350bd840906aa2c21b388
JulianWildauer <91659559+FriendlyPorcupine@users.noreply.github.com>
2023-01-13 12:58:51 +0100

    frontend

--- client old/index.js
+++ client old/index.js
@@ -2,28 +2,28 @@ const isAuthenticated = async () => {
   try {
     await axios.get('v1/user/me');
     return true;
-  } catch(_) {
+  } catch (_) {
     return false;
   }
-}
+};
 
 const initialize = async () => {
   const isAuth = await isAuthenticated();
 
   // Check if user is authenticated
   if (isAuth) {
-    document.querySelector("#login").style.display = "none";
-    document.querySelector("#logout").style.display = "block";
-    document.querySelector("#profile").style.display = "block";
+    document.querySelector('#login').style.display = 'none';
+    document.querySelector('#logout').style.display = 'block';
+    document.querySelector('#profile').style.display = 'block';
   } else {
     //if not logout and profile won't be visible
-    document.querySelector("#login").style.display = "block";
-    document.querySelector("#logout").style.display = "none";
-    document.querySelector("#profile").style.display = "none";
+    document.querySelector('#login').style.display = 'block';
+    document.querySelector('#logout').style.display = 'none';
+    document.querySelector('#profile').style.display = 'none';
   }
 
   // Signout functionality
-  document.querySelector("#logout").addEventListener("click", async (event) => {
+  document.querySelector('#logout').addEventListener('click', async (event) => {
     await axios.get('v1/user/signout');
     // once logout browser will refresh
     window.location.reload();
@@ -38,18 +38,17 @@ const initialize = async () => {
 
     const city = document.getElementById('city').value;
 
-    const res = await axios.get(
-      'v1/weather/city',
-      {
-        params: {
-          city,
-        },
+    const res = await axios.get('v1/weather/city', {
+      params: {
+        city,
       },
-    );
+    });
 
-    document.getElementById("temp-out").innerHTML = `In ${city} sind es ${res.data.temperature} °C`;
-    document.getElementById("temp-out").style.display = "block";
+    document.getElementById(
+      'temp-out',
+    ).innerHTML = `In ${city} sind es ${res.data.temperature} °C`;
+    document.getElementById('temp-out').style.display = 'block';
   });
-}
+};
 
 initialize();
