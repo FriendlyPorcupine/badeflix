245c2bea3d05fd76a915956ef6366961be3646ef
giuli <itsmegiuli@gmail.com>
2023-01-19 18:28:12 +0100

    homepage changed

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,13 +1,52 @@
-import { TodoList } from '../src/features/todo/components/TodoList';
-import HomePage from "./home/page";
+'use client';
+
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import axios from 'axios';
+import {useRef } from 'react';
+import { toast } from 'react-hot-toast';
+import {Container, Typography} from "@mui/material";
+import { useRouter } from "next/navigation";
 
 const IndexPage = () => {
-  return (
-    <div>
-      {/* @ts-ignore */}
-      <HomePage />
-    </div>
-  );
+    const addressRef = useRef<HTMLInputElement>();
+    const router = useRouter();
+
+    // @todo post to backend!
+    const search = async () => {
+
+      const addressToSearch = {
+        address: addressRef.current?.value,
+      };
+      console.log({addressToSearch})
+      try {
+        await axios.post('', addressToSearch);
+        router.push('/');
+      } catch (_) {
+        toast.error('There has been an error. Try again!')}
+    }
+
+    return (
+      <Container sx={{padding: 10}}>
+        <Typography variant="subtitle1">
+          To search for a bathing(? place, write your address in the search bar and click on search
+        </Typography>
+        <TextField
+          label="Search your address"
+          inputMode="search"
+          variant="filled"
+          margin="normal"
+          inputRef={addressRef}
+          fullWidth>
+        </TextField>
+        <Button variant="contained" onClick={search}>
+          Search
+        </Button>
+      </Container>
+
+
+    );
 };
+
 
 export default IndexPage;
