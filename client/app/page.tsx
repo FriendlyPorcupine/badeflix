fb3e36e9baf921ac8d507374856519f03bfa585e
KZoubek <91133180+KZoubek@users.noreply.github.com>
2023-01-21 13:27:45 +0100

    dummy table in index

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -19,6 +19,24 @@ import Paper from '@mui/material/Paper';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
 const IndexPage = () => {
   const addressRef = useRef<HTMLInputElement>();
   const router = useRouter();
@@ -78,7 +96,7 @@ const IndexPage = () => {
   };
 
   return (
-    <Container sx={{padding: 10}}>
+    <><Container sx={{padding: 10}}>
       <Typography variant="subtitle1">
         To search for a bathing(? place, write your address in the search bar and click on search
       </Typography>
@@ -93,10 +111,37 @@ const IndexPage = () => {
       <Button variant="contained" onClick={() => addressToGeoCode(addressRef.current?.value)}>
         Search
       </Button>
-
-    </Container>
-  );
-};
+    </Container><TableContainer component={Paper}>
+      <Table sx={{minWidth: 650}} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{'&:last-child td, &:last-child th': {border: 0}}}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer></>
+)
+}
 
 export default IndexPage;
 
