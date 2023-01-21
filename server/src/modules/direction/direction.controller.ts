4dac6470327291853e9ee6bf6955e0e998dea297
KZoubek <91133180+KZoubek@users.noreply.github.com>
2023-01-21 17:08:39 +0100

    Authguard disabled for postman in controller, renamed table except routing

--- server/src/modules/direction/direction.controller.ts
+++ server/src/modules/direction/direction.controller.ts
@@ -10,8 +10,8 @@ export class DirectionController {
   constructor(private readonly directionService: DirectionService) {}
 
   // DierctionService ist natürlich nur authentifizierten usern erlaubt
-  @UseGuards(AuthenticatedGuard)
-  
+  //@UseGuards(AuthenticatedGuard)
+
   // Wenn Auth dann get directions
   @Get('/direction')
   getDirections(
