3738925ddc0891ecb46da68f226f2e2766a8079c
JulianWildauer <91659559+FriendlyPorcupine@users.noreply.github.com>
2023-01-22 17:44:52 +0100

    weather fix

--- client/src/services/weather.service.ts
+++ client/src/services/weather.service.ts
@@ -3,13 +3,14 @@ export interface WeatherData {
   sunset: number;
 }
 
-export const fetchWeatherData = async (city: string): Promise<WeatherData> => {
+export const fetchWeatherData = async (city: string) => {
   const weatherRes = await fetch(
     'http://localhost:3000/v1/weather/city?' + new URLSearchParams({ city }),
   );
 
   if (!weatherRes.ok) {
-    throw new Error('Failed to fetch weather');
+    return undefined;
+    //throw new Error('Failed to fetch weather');
   }
 
   const weather: WeatherData = await weatherRes.json();
